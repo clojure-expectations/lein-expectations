8dead0e67bd5a0c5224996fbf1c25896912aab5d
Gareth Jones <gareth.e.jones@gmail.com>
2011-10-29 11:34:03 -0500

    added clojars

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
-(defproject lein-expectations "0.0.1"
+(defproject lein-expectations "0.0.2-SNAPSHOT"
   :description "Leiningen plugin to run tests written using the expectations library."
   :dependencies [[org.clojure/clojure "1.2.1"]]
-  :eval-in-leiningen true)
+  :eval-in-leiningen true
+  :repositories {"clojars" "http://clojars.org/repo"})
