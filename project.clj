f177ab480ab145a16a787206378fff9a2d1718eb
Gareth Jones <gareth.e.jones@gmail.com>
2011-10-28 23:12:34 -0500

    update version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-expectations "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject lein-expectations "0.0.1"
+  :description "Leiningen plugin to run tests written using the expectations library."
   :dependencies [[org.clojure/clojure "1.2.1"]]
   :eval-in-leiningen true)
