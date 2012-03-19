ef09b26dd5855d21acac1900d4013f47810e432b
Gareth Jones <gjones@drwholdings.com>
2012-03-19 06:35:26 -0500

    removing extra repo definition

--- project.clj
+++ project.clj
@@ -1,5 +1,4 @@
 (defproject lein-expectations "0.0.2"
   :description "Leiningen plugin to run tests written using the expectations library."
   :dependencies [[org.clojure/clojure "1.2.1"]]
-  :eval-in-leiningen true
-  :repositories {"clojars" "http://clojars.org/repo"})
+  :eval-in-leiningen true)
