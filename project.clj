cbeae8ef0a2423255c6ca033d369cc5420e208cc
Gareth Jones <gjones@drwholdings.com>
2012-03-19 06:35:58 -0500

    bump to next snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-expectations "0.0.2"
+(defproject lein-expectations "0.0.3-SNAPSHOT"
   :description "Leiningen plugin to run tests written using the expectations library."
   :dependencies [[org.clojure/clojure "1.2.1"]]
   :eval-in-leiningen true)
