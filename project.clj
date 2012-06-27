faf2388e0419fcd8758f9229949a104f54b2be51
Sean Corfield <sean@corfield.org>
2012-06-26 19:31:02 -0700

    Remove dependencies - should not need it with either Leiningen 1.7.1 or 2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,3 @@
 (defproject lein-expectations "0.0.5"
   :description "Leiningen plugin to run tests written using the expectations library."
-  :dependencies [[org.clojure/clojure "1.2.1"]]
   :eval-in-leiningen true)
