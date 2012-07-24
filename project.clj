0915bfdccbf09f8ac11a666575428397ee0d053c
Sean Corfield <sean@corfield.org>
2012-07-24 11:01:54 -0700

    Bump version for 0.0.7-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject lein-expectations "0.0.6"
+(defproject lein-expectations "0.0.7-SNAPSHOT"
   :description "Leiningen plugin to run tests written using the expectations library."
   :eval-in-leiningen true)
