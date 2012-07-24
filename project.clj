164b55fb33170764fe8784c70d16b6c88167bb38
Sean Corfield <sean@corfield.org>
2012-07-24 10:56:04 -0700

    Bump version for Leiningen 2 exit code fix

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject lein-expectations "0.0.5"
+(defproject lein-expectations "0.0.6"
   :description "Leiningen plugin to run tests written using the expectations library."
   :eval-in-leiningen true)
