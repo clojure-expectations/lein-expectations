a0a0d0efa5d08a0ee6a88dd786fe92351b5642c4
Sean Corfield <sean@corfield.org>
2012-08-06 12:08:31 -0700

    Add :url / :license to improve presentation on Clojars.

--- project.clj
+++ project.clj
@@ -1,3 +1,6 @@
 (defproject lein-expectations "0.0.8-SNAPSHOT"
   :description "Leiningen plugin to run tests written using the expectations library."
+  :url "https://github.com/gar3thjon3s/lein-expectations"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
