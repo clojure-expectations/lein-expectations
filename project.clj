bdf68590a690aad208115dc53fa898b93e441fac
Sean Corfield <sean@corfield.org>
2012-08-06 11:30:31 -0700

    Start 0.0.8-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject lein-expectations "0.0.7"
+(defproject lein-expectations "0.0.8-SNAPSHOT"
   :description "Leiningen plugin to run tests written using the expectations library."
   :eval-in-leiningen true)
